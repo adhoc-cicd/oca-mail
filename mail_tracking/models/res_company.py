cvinh/social

[IMP] Add option to keep aliases in mail tracking

mail_tracking/models/res_company.py
@@ -1,0 +1,10 @@
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    show_aliases_mail_tracking = fields.Boolean(
+        string="Show Aliases in Mail Tracking",
+        default=False,
+    )

adhoc-cicd/oca-mail

mail_tracking/models/res_company.py
@@ -1,0 +1,10 @@
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    mail_tracking_show_aliases = fields.Boolean(
+        string="Show Aliases in Mail Tracking",
+        default=False,
+    )